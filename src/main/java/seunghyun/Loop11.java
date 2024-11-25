32c69b45edc479445c454e294b72952ff1a276dd
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-11-25 18:55:23 +0900

    feat: 반복문 10952번 문제 구현

---
+++ src/main/java/seunghyun/Loop11.java
@@ -0,0 +1,19 @@
+package seunghyun;
+
+import java.util.Scanner;
+
+public class Loop11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if (A == 0 || B == 0) {
+                break;
+            }
+            sum = A + B;
+            System.out.println(sum);
+        }
+    }
+}
