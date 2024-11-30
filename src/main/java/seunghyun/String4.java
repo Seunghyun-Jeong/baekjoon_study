75ced556f934141bb3a373f08c8b61675bb259b6
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-12-01 00:36:14 +0900

    feat: 문자열 11654번 문제 구현

---
+++ src/main/java/seunghyun/String4.java
@@ -0,0 +1,11 @@
+package seunghyun;
+
+import java.util.Scanner;
+
+public class String4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char input = sc.next().charAt(0);
+        System.out.println((int) input);
+    }
+}
