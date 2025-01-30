2d4e22063fd1e74fa7aa66bbb290c556db5f5b2e
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-01-30 17:02:20 +0900

    feat: 백트래킹 15650번 문제 구현

---
+++ src/main/java/seunghyun/Backtracking2.java
@@ -0,0 +1,32 @@
+package seunghyun;
+
+import java.util.Scanner;
+
+public class Backtracking2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        if (!(1 <= M && M <= N && N <= 8)) {
+            throw new IllegalArgumentException();
+        }
+
+        int[] seq = new int[M];
+        generateSequence(N, M, 0, 1, seq);
+    }
+
+    public static void generateSequence(int N, int M, int depth, int start, int[] seq) {
+        if (depth == M) {
+            for (int num : seq) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            seq[depth] = i;
+            generateSequence(N, M, depth + 1, i + 1, seq);
+        }
+    }
+}
