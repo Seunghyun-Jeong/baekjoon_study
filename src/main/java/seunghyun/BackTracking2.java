80ab9e5b54628d5e95461d307d2f899d4437082f
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-02-01 20:29:46 +0900

    rename: 백트래킹 이름 변경

--- src/main/java/seunghyun/BackTracking2.java
+++ src/main/java/seunghyun/BackTracking2.java
@@ -2,7 +2,7 @@ package seunghyun;
 
 import java.util.Scanner;
 
-public class Backtracking2 {
+public class BackTracking2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
--- src/main/java/seunghyun/BackTracking3.java
+++ src/main/java/seunghyun/BackTracking3.java
@@ -2,7 +2,7 @@ package seunghyun;
 
 import java.util.Scanner;
 
-public class Backtracking3 {
+public class BackTracking3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
