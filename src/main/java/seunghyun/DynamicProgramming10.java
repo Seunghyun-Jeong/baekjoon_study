c056d5bd70306efcf7d5864ea260b37d824bbff0
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-02-15 18:51:10 +0900

    feat: 동적계획법 10844번 문제 구현

---
+++ src/main/java/seunghyun/DynamicProgramming10.java
@@ -0,0 +1,38 @@
+package seunghyun;
+
+import java.util.Scanner;
+
+public class DynamicProgramming10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (1 > N || N > 100) {
+            throw new IllegalArgumentException();
+        }
+
+        int MOD = 1000000000;
+        long[][] dp = new long[N + 1][10];
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j <= 9; j++) {
+                if (j > 0) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                }
+                if (j < 9) {
+                    dp[i][j] += dp[i - 1][j + 1];
+                }
+                dp[i][j] %= MOD;
+            }
+        }
+
+        long result = 0;
+        for (int i = 0; i <= 9; i++) {
+            result = (result + dp[N][i]) % MOD;
+        }
+
+        System.out.println(result);
+    }
+}
